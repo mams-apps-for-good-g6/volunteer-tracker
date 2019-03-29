d25ee6484a2487771c35382fa9c0b27d1110882a
Sriya <siri.pidatala@gmail.com>
2019-03-29 11:45:47 -0400

    Sriya

---
+++ app/src/main/java/com/example/appsforgood/DisplayCode.java
@@ -0,0 +1,14 @@
+package com.example.appsforgood;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DisplayCode extends AppCompatActivity
+{
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.display_code);
+    }
+}
