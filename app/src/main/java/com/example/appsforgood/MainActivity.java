f962bbbc2017cb7ff88e71c60965c0ae391dc395
meganfrisella <mmfrisella@wpi.edu>
2019-03-29 11:48:14 -0400

    Megan

--- app/src/main/java/com/example/appsforgood/MainActivity.java
+++ app/src/main/java/com/example/appsforgood/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DatabaseReference;
@@ -34,6 +35,26 @@ public class MainActivity extends AppCompatActivity {
     public void toAdvisorSignUp(View v)
     {
         Intent intent = new Intent(this, AdvisorSignUp.class);
+        startActivity(intent);
+    }
+
+    public void displayCode(View v)
+    {
+        // take input and generate code by creating an organization object and calling the generateCode method
+        EditText OrgName = findViewById(R.id.organizationName);
+        EditText first = findViewById(R.id.firstName);
+        EditText last = findViewById(R.id.lastName);
+        EditText email = findViewById(R.id.email);
+
+        String OrgNameStr = OrgName.getText().toString();
+        String firstStr = first.getText().toString();
+        String lastStr = last.getText().toString();
+        String emailStr = email.getText().toString();
+
+        Organization org = new Organization(OrgNameStr, new User(firstStr, lastStr, emailStr));
+        int code = org.getCode();
+        Intent intent = new Intent(this, DisplayCode.class);
+        intent.putExtra("organizationCode", code);
         startActivity(intent);
     }
 }
