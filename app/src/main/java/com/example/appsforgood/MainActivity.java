de3b883b2d97e04206c6d215283ea48b48304fca
meganfrisella <mmfrisella@wpi.edu>
2019-04-03 09:45:00 -0400

    Megan

--- app/src/main/java/com/example/appsforgood/MainActivity.java
+++ app/src/main/java/com/example/appsforgood/MainActivity.java
@@ -4,16 +4,18 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
