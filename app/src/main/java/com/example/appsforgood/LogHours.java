0d8b4310a6b2f182d0b86c7d8736891525723037
evangaus <epgaus@wpi.edu>
2019-04-23 11:48:51 -0400

    Evan

--- app/src/main/java/com/example/appsforgood/LogHours.java
+++ app/src/main/java/com/example/appsforgood/LogHours.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 public class LogHours extends AppCompatActivity
 {
@@ -15,7 +16,24 @@ public class LogHours extends AppCompatActivity
 
     public void logHours(View v)
     {
-        // DO STUFF HERE TO ADD HOURS TO HOUR LOG
+        // Get the inputted values
+        EditText charityName = findViewById(R.id.charityName);
+        EditText hours = findViewById(R.id.hours);
+        EditText date = findViewById(R.id.date);
+        EditText contactPerson = findViewById(R.id.contactPerson);
+        EditText contactEmail = findViewById(R.id.contactEmail);
+
+        String charityNameStr = charityName.getText().toString();
+        Double hoursDouble = Double.parseDouble(hours.getText().toString());
+        String dateStr = date.getText().toString();
+        String contactPersonStr = contactPerson.getText().toString();
+        String contactEmailStr = contactEmail.getText().toString();
+
+        // Create a new LogEntry from the inputted data
+        LogEntry log = new LogEntry(charityNameStr, hoursDouble, dateStr, contactPersonStr, contactEmailStr);
+
+        //Send it off to firebase
+
 
         // After hours are logged, user is sent to their hour log
         Intent intent = new Intent(this, HourLog.class);
