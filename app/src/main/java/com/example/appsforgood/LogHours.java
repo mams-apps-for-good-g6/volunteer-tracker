1ab0d501ce07a15e3fefa0a388b985e267a752f6
evangaus <epgaus@wpi.edu>
2019-04-24 13:14:59 -0400

    Merge branch 'master' of C:\Users\evang\volunteer-tracker with conflicts.

--- app/src/main/java/com/example/appsforgood/LogHours.java
+++ app/src/main/java/com/example/appsforgood/LogHours.java
@@ -3,8 +3,15 @@ package com.example.appsforgood;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LogHours extends AppCompatActivity
 {
@@ -34,6 +41,34 @@ public class LogHours extends AppCompatActivity
 
         //Send it off to firebase
 
+        // THIS IS A BIG PASTE HERE
+
+        final int check = 5; // This it the index of the signed in volunteer, which we would need to get
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference("organizations/-Ld9uKTam5hwU1uK0CFb/volunteers");
+
+        // Attach a listener to read the data at the reference
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot ds : dataSnapshot.getChildren()) {
+                    Volunteer vol = ds.getValue(Volunteer.class);
+                    Log.d("MeganTag", "We are here #1");
+                    if (vol.getIndex() == check) {
+                        //vol.addTheLogEntryHere
+                        ds.getRef().setValue(vol);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+            }
+        });
+
+        // END OF THE BIG PASTE HERE
 
 
         // After hours are logged, user is sent to their hour log
