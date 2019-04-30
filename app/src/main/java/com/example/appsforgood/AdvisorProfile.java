68bcd1557e00556057edfa0350993a39a3d995ad
evangaus <epgaus@wpi.edu>
2019-04-30 14:43:01 -0400

    Evan

--- app/src/main/java/com/example/appsforgood/AdvisorProfile.java
+++ app/src/main/java/com/example/appsforgood/AdvisorProfile.java
@@ -13,6 +13,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,6 +21,7 @@ public class AdvisorProfile extends AppCompatActivity
 {
     private String TAG = "EvanTag";
     private String orgPath = "";
+    private ArrayList<LogEntry> verifyLogsList = new ArrayList<>();
 
     private void setKey(String k)
     {
@@ -73,6 +75,57 @@ public class AdvisorProfile extends AppCompatActivity
 
     public void toHourLog(View v) {
         Intent intent = new Intent(this, HourLogRecyclerView.class);
+        startActivity(intent);
+    }
+
+    public void toVerifyList(View v) {
+        //Intent intent = new Intent(this, HourLog.class);
+        //startActivity(intent);
+        Log.d("EvanTag", "OrgPath is: " + orgPath);
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference ref = database.getReference("organizations/");
+
+        // Attach a listener to read the data at the reference
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for(DataSnapshot datasnapshot: dataSnapshot.getChildren()) {
+                    Organization org = datasnapshot.getValue(Organization.class);
+                    Log.d(TAG, org.toString());
+
+                    ArrayList<Volunteer> volunteers = org.getVolunteers();
+
+                    for(Volunteer vol : volunteers)
+                    {
+                        for(LogEntry log : vol.getLogEntries())
+                        {
+                            if(log.getApprovalStatus() == 0)
+                            {
+                                verifyLogsList.add(log);
+                            }
+                        }
+                    }
+
+                    // Now we have the arraylist full or LogEntries that are pending
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+                Log.d(TAG,"Checkpoint #7");
+            }
+        });
+
+
+        // This is still the same method
+
+        // Leave this class, and go to a new XML that's a recycler view of the arraylist I'm sending it
+        Intent intent = new Intent(this, VerifyHours.class);
+        intent.putExtra("verifyLogsList", verifyLogsList);
         startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/appsforgood/VerifyHours.java
@@ -0,0 +1,25 @@
+package com.example.appsforgood;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class VerifyHours extends AppCompatActivity
+{
+
+
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.verify_hours);
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        ArrayList<LogEntry> verifyLogsList = (ArrayList<LogEntry>) bundle.getSerializable("verifyLogsList");
+
+        // Recycle view this
+    }
+
+}
